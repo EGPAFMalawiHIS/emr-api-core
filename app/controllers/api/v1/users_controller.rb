26b69b99bd28ac0b6ab9d74b9808fc6931741845
bryan <brianmsyamboza@gmail.com>
2023-07-17 14:20:25 +0200

    void user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -30,21 +30,27 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def update
-    @user.update(user_params)
-    respond_with(@user)
+    user_params = params.permit(:username)
+    person_params = params.require(:person).permit(
+      :gender, :birthdate, :birthdate_estimated, :creator, 
+      names: [:given_name, :family_name], 
+      addresses: [:address1, :city_village, :country, :postal_code]
+    )
+
+    role_params = params.require(:roles).map { |role| role.permit(:name, :description) }
+
+    updated_user = UserService.update_user(@user, user_params, person_params, role_params)
+
+    render json: updated_user, status: :ok
   end
 
   def destroy
-    @user.destroy
-    respond_with(@user)
+    @user.void(params[:reason])
+    respond_with("User voided successfully")
   end
 
   private
     def set_user
       @user = User.find_by_uuid(params[:id])
-    end
-
-    def user_params
-      params.require(:user).permit(:system_id, :username, :password, :salt, :secret_question, :secret_answer, :creator, :date_created, :changed_by, :date_changed, :person_id, :retired, :retired_by, :date_retired, :retire_reason, :uuid, :activation_key, :email)
     end
 end
--- app/services/user_service.rb
+++ app/services/user_service.rb
@@ -21,16 +21,39 @@ module UserService
             user.save!
             
             # Create roles and associate them with the user
-            roles = role_params.to_a.map { |role| Role.find_by_role(role['name']) }
-            
-            roles.each do |role|
-                user_role = UserRole.new
-                user_role.user = user
-                user_role.role = role
-                user_role.save!
-            end
+            assign_user_roles(user, role_params)
 
             user
+        end
+    end
+
+    def self.update_user(user, user_params, person_params, role_params)
+        ActiveRecord::Base.transaction do
+            updated_person = PersonService.update_person(user.person, person_params)
+
+            updated_person_name = PersonNameService.update_person_name(updated_person, person_params)
+
+            address_params = person_params.delete(:addresses).first.to_h
+            updated_person_address = PersonAddressService.update_person_address(updated_person, address_params)
+
+            user.username = user_params[:username] if user_params[:username].present?
+            
+            assign_user_roles(user, role_params)
+
+            user.save!
+
+            user
+        end
+    end
+
+    private
+
+    def assign_user_roles(user, params)
+        # re assign roles
+        UserRole.where(user: user).destroy_all
+        params.each do |role|
+            role = Role.find_by_role(role['name'])
+            UserRole.find_or_create_by(user: user, role: role)
         end
     end
 end
