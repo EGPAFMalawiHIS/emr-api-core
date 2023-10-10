b665cec8156488139886ac3a9ede74ef0e099de6
Roy Chanunkha <roychanunkha@gmail.com>
2023-10-10 13:19:46 +0200

    lints

--- app/models/encounter_role.rb
+++ app/models/encounter_role.rb
@@ -8,7 +8,7 @@ class EncounterRole < ApplicationRecord
   belongs_to :user, class_name: 'User', foreign_key: :creator, primary_key: :user_id, optional: true
   belongs_to :forcer, class_name: 'User', foreign_key: :retired_by, primary_key: :user_id, optional: true
   has_many :encounter_provider, foreign_key: encounter_role_id, primary_key: encounter_role_id
-  
+
   validates :name, presence: true
   validates :creator, presence: true
   validates :date_created, presence: true
